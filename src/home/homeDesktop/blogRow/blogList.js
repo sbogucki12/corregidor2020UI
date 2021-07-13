79aeddb1220fddd4d756c50811c842932d2592bb
Steve B <sbogucki@mail.usf.edu>
2021-07-13 07:40:01 -0700

    added blog entry to array

--- src/home/homeDesktop/blogRow/blogList.js
+++ src/home/homeDesktop/blogRow/blogList.js
@@ -1,27 +1,34 @@
 const blogList = [
     {
         id: 0, 
+        title: "Setting Up and Azure Function (SendGrid / HTTP Trigger)", 
+        url: "https://bogoodski.medium.com/setting-up-an-azure-function-sendgrid-http-trigger-cfd9c5791201", 
+        author: "Steve B.",
+        publicationDate: "Jul. 2, 2021"
+    },
+    {
+        id: 1, 
         title: "Cloud-Based System Architecture for the Federal Domain", 
         url: "https://bogoodski.medium.com/cloud-based-system-architecture-for-the-federal-domain-ec77d12e4608", 
         author: "Steve B.",
         publicationDate: "Jan. 5, 2021"
     },
     {
-        id: 1, 
+        id: 2, 
         title: "Agile Software Development in the Federal Government Domain", 
         url: "https://medium.com/digital-diplomacy/agile-software-development-in-the-federal-government-domain-c6fa87b7569d?source=friends_link&sk=66a9aa487362de98783b8535b824baa0", 
         author: "Steve B.",
         publicationDate: "Dec. 28, 2020"
     },
     {
-        id: 2, 
+        id: 3, 
         title: "The Future of Federal Safety Oversight", 
         url: "https://bogoodski.medium.com/the-future-of-federal-safety-oversight-6992b3012740", 
         author: "Steve B.",
         publicationDate: "Dec. 21, 2020"
     },
     {
-        id: 3, 
+        id: 4, 
         title: "Migrating from MongoDB to SQL", 
         url: "https://medium.com/swlh/migrating-from-mongodb-to-sql-b2d91d13c30d?source=friends_link&sk=7420676c3fbb0cfbb94dfb1e9cc02ccd", 
         author: "Steve B.",
